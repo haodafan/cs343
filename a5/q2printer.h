c8209b0c4dad35c853a2df4cbf3d48d36ea6320e
haodafan <haoda.fan@outlook.com>
2020-03-26 16:23:05 -0400

    Changed printer .h file to match a4

--- a5/q2printer.h
+++ a5/q2printer.h
@@ -7,11 +7,25 @@ _Monitor Printer {
 #else 
 _Cormonitor Printer {             // chose one of the two kinds of type constructor
 #endif
-    unsigned int voters; 
+    const unsigned int Voters, Undefined;
+    unsigned int prev;					// previous inserter into the buffer
+    unsigned int cnt;					// number of filled buffer elements
+
+    // Defines information that needs to be tracked.
+    struct Items {
+	unsigned int id;
+	Voter::States state;
+	union {
+	    TallyVotes::Ballot ballot;
+	    unsigned int numBlocked;
+	    TallyVotes::Tour tour;
+	};
+    } curr, *columns;                                   // curr is the current incoming information, columns is the stored information.
   public:
-    Printer( unsigned int voters ) : voters(voters) {} 
+    Printer( unsigned int voters );
+    ~Printer();
     void print( unsigned int id, Voter::States state );
+    void print( unsigned int id, Voter::States state, TallyVotes::Ballot vote );
     void print( unsigned int id, Voter::States state, TallyVotes::Tour tour );
-    void print( unsigned int id, Voter::States state, TallyVotes::Ballot ballot );
     void print( unsigned int id, Voter::States state, unsigned int numBlocked );
-};
+}; // Printer
