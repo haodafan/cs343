79d98f9b98f52a67ad4dd62a0d3c88756891ae81
haodafan <haoda.fan@outlook.com>
2020-03-26 16:54:52 -0400

    Compilation fix attempt 15 - for some reason i deleted q2tallyvotes.h, put it back in this commit

---
+++ a5/q2tallyvotes.h
@@ -0,0 +1,66 @@
+#pragma once 
+
+#if defined( EXT ) || ( INT )
+_Monitor Printer;
+#else 
+_Cormonitor Printer;
+#endif 
+
+#if defined( EXT )                           // external scheduling monitor solution
+// includes for this kind of vote-tallier
+_Monitor TallyVotes {
+    // private declarations for this kind of vote-tallier
+
+#elif defined( INT )                         // internal scheduling monitor solution
+// includes for this kind of vote-tallier
+_Monitor TallyVotes {
+    // private declarations for this kind of vote-tallier
+#elif defined( INTB )                        // internal scheduling monitor solution with barging
+// includes for this kind of vote-tallier
+_Monitor TallyVotes {
+    // private declarations for this kind of vote-tallier
+    uCondition bench;                        // only one condition variable (variable name may be changed)
+    void wait();                             // barging version of wait
+    void signalAll();                        // unblock all waiting tasks
+#elif defined( AUTO )                        // automatic-signal monitor solution
+// includes for this kind of vote-tallier
+_Monitor TallyVotes {
+    // private declarations for this kind of vote-tallier
+#elif defined( TASK )                        // internal/external scheduling task solution
+_Task TallyVotes {
+    // private declarations for this kind of vote-tallier
+#else
+    #error unsupported voter type
+#endif
+    // common declarations
+
+    // constructor constants 
+    unsigned int voters; 
+    unsigned int group; 
+    Printer & printer; 
+
+    // counters 
+    unsigned int groupno = 0;
+    unsigned int count = 0; // count represents the number of voters currently voting
+    unsigned int barging = 0;  // barging represents the number of voters not yet voting (barging avoidance)
+    //unsigned int barging = 0;
+
+    // Trip data 
+    int votesP = 0; 
+    int votesS = 0; 
+    int votesG = 0; 
+    unsigned int groupid = 0;
+    char currtk;
+
+  public:                                    // common interface
+    _Event Failed {};
+    TallyVotes( unsigned int voters, unsigned int group, Printer & printer )
+        : voters(voters), group(group), printer(printer) {}
+    struct Ballot { unsigned int picture, statue, giftshop; };
+    enum TourKind { Picture = 'p', Statue = 's', GiftShop = 'g' };
+    struct Tour { TourKind tourkind; unsigned int groupno; };
+    Tour vote( unsigned int id, Ballot ballot );
+    void done();
+};
+
+
